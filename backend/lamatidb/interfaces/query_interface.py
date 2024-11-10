bf04e1a1145a482f532f8ecaade30094ebe6bc3a
datagero <datagero@outlook.com>
2024-11-09 19:24:52 -0600

    updated branch to new apis

--- backend/lamatidb/interfaces/query_interface.py
+++ backend/lamatidb/interfaces/query_interface.py
@@ -5,11 +5,11 @@ from llama_index.core.retrievers import VectorIndexRetriever, QueryFusionRetriev
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.postprocessor import SimilarityPostprocessor
 from llama_index.core import VectorStoreIndex, get_response_synthesizer
+from llama_index.core.vector_stores import FilterCondition
 from llama_index.core.vector_stores.types import MetadataFilter, MetadataFilters
 from llama_index.core import Settings
 from llama_index.core.llms import ChatMessage
 from llama_index.llms.together import TogetherLLM
-
 
 class QueryInterface:
     def __init__(self, index):
@@ -38,9 +38,9 @@ class QueryInterface:
         )
         return QUERY_GEN_PROMPT
     
-    def configure_retriever(self, similarity_top_k=100, metadata_filters=None):
+    def configure_retriever(self, similarity_top_k=100, metadata_filters=None, condition=FilterCondition.OR):
         if metadata_filters:
-            metadata_filters = MetadataFilters(filters=[MetadataFilter(**f) for f in metadata_filters])
+            metadata_filters = MetadataFilters(filters=[MetadataFilter(**f) for f in metadata_filters], condition=condition)
     
         self.retriever = VectorIndexRetriever(
             index=self.index,
